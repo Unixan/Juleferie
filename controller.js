6d9cb7ff098facdb6322cfbfc399041c9be419c1
Unixan <115973884+Unixan@users.noreply.github.com>
2022-12-14 12:31:49 +0100

    User name uniqe?
    Added check if username allready qxists function

--- controller.js
+++ controller.js
@@ -1,16 +1,18 @@
 function logInScreen() {
   model.mainView = "";
   model.mainView += /*HTML*/ `
-<h1>Welcome!</h1>
-<div class="dialogueBox">
-    <div class="inputFields">
-        <label for="loginName">Username:</label><br>
-        <input type="text" id="loginName"/><br><br>
-        <label for="loginPword">Password:</label><br>
-        <input type="password" id="loginPword"/><br><br>
-        </div>
-        <button onclick="loginCheck()">Log in</button> or <button onclick="createAccount()">Create new account</button>
-        <p>${model.error}</p>
+<div class="login">
+  <h1>Welcome!</h1>
+  <div class="dialogueBox">
+      <div class="inputFields">
+          <label for="loginName">Username:</label><br>
+          <input type="text" id="loginName"/><br><br>
+          <label for="loginPword">Password:</label><br>
+          <input type="password" id="loginPword"/><br><br>
+          </div>
+          <button onclick="loginCheck()">Log in</button> or <button onclick="createAccount()">Create new account</button>
+          <p>${model.error}</p>
+  </div>
 </div>
   `;
   model.error = "";
@@ -20,16 +22,18 @@ function createAccount() {
   model.createAccount = true;
   model.mainView = "";
   model.mainView += /*HTML*/ `
+<div class="login">
   <h1>Create new account</h1>
-<div class="dialogueBox">
-    <label for="newName">Username:</label><br>
-    <input type="text" id="newName" placeholder="Minimum 3 characters"/><br><br>
-    <label for="newPword">Password:</label><br>
-    <input type="password" id="newPword" placeholder="Minimum 8 characters"/><br><br>
-    <label for="newPwordVerify">Verify password:</label><br>
-    <input type="password" id="newPwordVerify" placeholder="Minimum 8 characters"/><br><br>
-    <button onclick="createNewAccount()">Create account</button> <button onclick="cancelAccountCreate()">Cancel</button></p>
-    ${model.error}
+  <div class="dialogueBox">
+      <label for="newName">Username:</label><br>
+      <input type="text" id="newName" placeholder="Minimum 3 characters"/><br><br>
+      <label for="newPword">Password:</label><br>
+      <input type="password" id="newPword" placeholder="Minimum 8 characters"/><br><br>
+      <label for="newPwordVerify">Verify password:</label><br>
+      <input type="password" id="newPwordVerify" placeholder="Minimum 8 characters"/><br><br>
+      <button onclick="createNewAccount()">Create account</button> <button onclick="cancelAccountCreate()">Cancel</button></p>
+      ${model.error}
+  </div>
 </div>
   `;
   model.error = "";
@@ -48,7 +52,6 @@ function createNewAccount() {
     userLevel: "user",
   };
   let verify = document.getElementById("newPwordVerify").value;
-  console.log(verify, newUser.userPword);
   if (newUser.userName.length < 3) {
     model.error = `<div style="color: red; font-size: 10">Username must be atleast 3 characters long</div><br>`;
     createAccount();
@@ -58,10 +61,13 @@ function createNewAccount() {
   } else if (newUser.userPword != verify) {
     model.error = `<div style="color: red; font-size: 10">Passwords doesn't match</div><br>`;
     createAccount();
+  } else if (model.userAccounts.find((e) => e.userName === newUser.userName)) {
+    model.error = `<div style="color: red; font-size: 10">Username not available</div><br>`;
+    createAccount();
   } else {
     model.userAccounts.push(newUser);
-    console.log(model.userAccounts);
     model.createAccount = false;
+    model.error = `<div>Account creation successfull</div>`;
     view();
   }
 }
@@ -84,8 +90,8 @@ function loginCheck() {
   if (loginSuccess == false) {
     model.error = `<div style="color: red; font-size: 10">Username or password is wrong</div><br>`;
     model.loginCheck = false;
-  }else{
-    model.mainView = `<h1>Welcome ${userName}!</h1>`
+  } else {
+    model.mainView = `<h1>Welcome ${userName}!</h1>`;
   }
   view();
 }
